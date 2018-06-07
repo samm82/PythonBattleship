bf855459fbf97ef4ef961a6fb269ee836d5fcce2
Sam Crawford <sam.crawford82@gmail.com>
2018-06-07 16:28:16 -0400

    Initialization/title screen

---
+++ battleship.py
@@ -0,0 +1,22 @@
+##Battleship
+
+import random as r
+
+def main():
+    print("BBBBB   AAAA  TTTTTT TTTTTT LL     EEEEEE  SSSS  HH  HH IIIIII PPPPP \nBB  BB AA  AA   TT     TT   LL     EE     SS  SS HH  HH   II   PP  PP\nBBBBB  AA  AA   TT     TT   LL     EEEE    SS    HHHHHH   II   PP  PP\nBB  BB AAAAAA   TT     TT   LL     EE        SS  HH  HH   II   PPPPP \nBB  BB AA  AA   TT     TT   LL     EE     SS  SS HH  HH   II   PP    \nBBBBB  AA  AA   TT     TT   LLLLLL EEEEEE  SSSS  HH  HH IIIIII PP ")
+    if input("\nPlay a game? ")[0].lower() == 'y':
+        play()
+    else:
+        raise SystemExit
+
+def play():
+    size = 5
+    makeGrid(size)
+    
+def makeGrid(s):
+    boatPos = []
+    x, y = r.randint(0,5), r.randint(0,5)
+    boatPos.append((x, y))
+    grid = []
+
+main()
