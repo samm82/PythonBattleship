814d53b4cd452b49d6efb2819a915d1d770c1795
Sam Crawford <sam.crawford82@gmail.com>
2018-06-08 23:45:20 -0400

    Added empty answerFull.py

--- battleship.py
+++ battleship.py
@@ -1,7 +1,8 @@
 ##Battleship
 
 import random as r
-import answer as a
+import answerTen as aT
+import answerFull as aF
 
 def main():
     print("BBBBB   AAAA  TTTTTT TTTTTT LL     EEEEEE  SSSS  HH  HH IIIIII PPPPP \nBB  BB AA  AA   TT     TT   LL     EE     SS  SS HH  HH   II   PP  PP\nBBBBB  AA  AA   TT     TT   LL     EEEE    SS    HHHHHH   II   PP  PP\nBB  BB AAAAAA   TT     TT   LL     EE        SS  HH  HH   II   PPPPP \nBB  BB AA  AA   TT     TT   LL     EE     SS  SS HH  HH   II   PP    \nBBBBB  AA  AA   TT     TT   LLLLLL EEEEEE  SSSS  HH  HH IIIIII PP ")
