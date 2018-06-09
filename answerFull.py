1e4b53f9bf39996ee0ec1c38af72863fd4f70d33
Sam Crawford <sam.crawford82@gmail.com>
2018-06-09 14:43:14 -0400

    Placement of three long ships works; closes #8

--- answerFull.py
+++ answerFull.py
@@ -1,36 +1,89 @@
 import random as r
 
 def blankGrid():
-    grid = [['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
-            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
-            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
-            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
-            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
-            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
-            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
-            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
-            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
-            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O']]
-    return grid
+    blank = [['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+             ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+             ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+             ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+             ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+             ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+             ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+             ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+             ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+             ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O']]
+    return blank
 
-def threeLong(s):
+#FOR TESTING
+def display(lst):
+    for line in lst:
+        for i in line:
+            print(i, end=' ')
+        print()
+
+def fullGen():
+    grid1 = twoLong()
+    print(grid1)
+    grid2 = threeLong(grid1)
+    display(grid2)
+    grid3 = threeLong(grid2)
+    display(grid3)
+
+def twoLong():
     gridList = []
-    for i in range(s):
-        for j in range(s):
-            gridList = horizontalThree(i, j, s, gridList)
-            gridList = verticalThree(i, j, s, gridList)
-    return gridList
+    for i in range(10):
+        for j in range(10):
+            gridList = horizontalTwo(i, j, gridList)
+            gridList = verticalTwo(i, j, gridList)
+    return r.choice(gridList)
 
-def horizontalThree(x, y, s, gL):
+def threeLong(grid):
+    gridList = []
+    for i in range(10):
+        for j in range(10):
+            gridList = horizontalThree(i, j, gridList, grid)
+            gridList = verticalThree(i, j, gridList, grid)
+    return r.choice(gridList)
+
+##TWO
+def horizontalTwo(x, y, gL):
     grid = blankGrid()
-    if x+2 < s and x+1 < s: 
-        grid[x][y], grid[x+1][y], grid[x+2][y] = 'X', 'X', 'X'
+    if x+1 < 10: 
+        grid[x][y], grid[x+1][y] = 'X', 'X'
+        grid.append([[x, y], [x+1, y]]) #appends "X" coordinates
         gL.append(grid)
     return gL
 
-def verticalThree(x, y, s, gL):
+def verticalTwo(x, y, gL):
     grid = blankGrid()
-    if y+2 < s and y+1 < s:
+    if y+1 < 10:
+        grid[x][y], grid[x][y+1] = 'X', 'X'
+        grid.append([[x, y], [x, y+1]]) #appends "X" coordinates
+        gL.append(grid)
+    return gL 
+
+##THREE
+
+def initialize(coords):
+    grid = blankGrid()
+    for coord in coords:
+        grid[coord[0]][coord[1]] = "X"
+    grid.append(coords)
+    return grid
+
+def horizontalThree(x, y, gL, g):
+    grid = initialize(g[-1])
+    if x+2 < 10 and x+1 < 10 and not any(i in [[x, y], [x+1, y], [x+2, y]] for i in grid[-1]): 
+        grid[x][y], grid[x+1][y], grid[x+2][y] = 'X', 'X', 'X'
+        grid[-1] = grid[-1]+[[x, y], [x+1, y], [x+2, y]]
+        gL.append(grid)
+    return gL
+
+def verticalThree(x, y, gL, g):
+    grid = initialize(g[-1])
+    if y+2 < 10 and y+1 < 10 and not any(i in [[x, y], [x, y+1], [x, y+2]] for i in grid[-1]): 
         grid[x][y], grid[x][y+1], grid[x][y+2] = 'X', 'X', 'X'
+        grid[-1] = grid[-1]+[[x, y], [x, y+1], [x, y+2]]
         gL.append(grid)
     return gL    
+
+fullGen()
