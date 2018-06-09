31c76b41a8474b3274a2cb92aabd4d9754c6b63f
Sam Crawford <sam.crawford82@gmail.com>
2018-06-09 14:52:33 -0400

    Added fourLong

--- answerFull.py
+++ answerFull.py
@@ -20,10 +20,19 @@ def display(lst):
             print(i, end=' ')
         print()
 
+def initialize(coords):
+    grid = blankGrid()
+    for coord in coords:
+        grid[coord[0]][coord[1]] = "X"
+    grid.append(coords)
+    return grid
+
 def fullGen():
     grid1 = twoLong()
     grid2 = threeLong(grid1)
     grid3 = threeLong(grid2)
+    grid4 = fourLong(grid3)
+    display(grid4)
 
 def twoLong():
     gridList = []
@@ -39,6 +48,14 @@ def threeLong(grid):
         for j in range(10):
             gridList = horizontalThree(i, j, gridList, grid)
             gridList = verticalThree(i, j, gridList, grid)
+    return r.choice(gridList)
+
+def fourLong(grid):
+    gridList = []
+    for i in range(10):
+        for j in range(10):
+            gridList = horizontalFour(i, j, gridList, grid)
+            gridList = verticalFour(i, j, gridList, grid)
     return r.choice(gridList)
 
 ##TWO
@@ -60,13 +77,6 @@ def verticalTwo(x, y, gL):
 
 ##THREE
 
-def initialize(coords):
-    grid = blankGrid()
-    for coord in coords:
-        grid[coord[0]][coord[1]] = "X"
-    grid.append(coords)
-    return grid
-
 def horizontalThree(x, y, gL, g):
     grid = initialize(g[-1])
     if x+2 < 10 and x+1 < 10 and not any(i in [[x, y], [x+1, y], [x+2, y]] for i in grid[-1]): 
@@ -81,6 +91,24 @@ def verticalThree(x, y, gL, g):
         grid[x][y], grid[x][y+1], grid[x][y+2] = 'X', 'X', 'X'
         grid[-1] = grid[-1]+[[x, y], [x, y+1], [x, y+2]] #adds "X" coordinates to previous list
         gL.append(grid)
-    return gL    
+    return gL
+
+##FOUR
+
+def horizontalFour(x, y, gL, g):
+    grid = initialize(g[-1])
+    if x+3 < 10 and x+2 < 10 and x+1 < 10 and not any(i in [[x, y], [x+1, y], [x+2, y], [x+3, y]] for i in grid[-1]): 
+        grid[x][y], grid[x+1][y], grid[x+2][y], grid[x+3][y] = 'X', 'X', 'X', 'X'
+        grid[-1] = grid[-1]+[[x, y], [x+1, y], [x+2, y], [x+3, y]] #adds "X" coordinates to previous list
+        gL.append(grid)
+    return gL
+
+def verticalFour(x, y, gL, g):
+    grid = initialize(g[-1])
+    if y+3 <10 and y+2 < 10 and y+1 < 10 and not any(i in [[x, y], [x, y+1], [x, y+2], [x, y+3]] for i in grid[-1]): 
+        grid[x][y], grid[x][y+1], grid[x][y+2], grid[x][y+3] = 'X', 'X', 'X', 'X'
+        grid[-1] = grid[-1]+[[x, y], [x, y+1], [x, y+2], [x, y+3]] #adds "X" coordinates to previous list
+        gL.append(grid)
+    return gL   
 
 fullGen()
