2ee16ecd1c17dad21defb1170c8a25abbb69aa92
Sam Crawford <sam.crawford82@gmail.com>
2018-06-09 14:01:31 -0400

    Implemented bare-bones answerFull

--- answerFull.py
+++ answerFull.py
@@ -1,1 +1,36 @@
 import random as r
+
+def blankGrid():
+    grid = [['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O']]
+    return grid
+
+def threeLong(s):
+    gridList = []
+    for i in range(s):
+        for j in range(s):
+            gridList = horizontalThree(i, j, s, gridList)
+            gridList = verticalThree(i, j, s, gridList)
+    return gridList
+
+def horizontalThree(x, y, s, gL):
+    grid = blankGrid()
+    if x+2 < s and x+1 < s: 
+        grid[x][y], grid[x+1][y], grid[x+2][y] = 'X', 'X', 'X'
+        gL.append(grid)
+    return gL
+
+def verticalThree(x, y, s, gL):
+    grid = blankGrid()
+    if y+2 < s and y+1 < s:
+        grid[x][y], grid[x][y+1], grid[x][y+2] = 'X', 'X', 'X'
+        gL.append(grid)
+    return gL    
