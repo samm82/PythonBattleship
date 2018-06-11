40803b79fe8dcf0a866461bf47fc689f5475e86c
Sam Crawford <sam.crawford82@gmail.com>
2018-06-11 15:40:30 -0400

    Added displayBoth() and blankComp

--- display.py
+++ display.py
@@ -3,3 +3,9 @@ def display(lst):
         for i in line:
             print(i, end=' ')
         print()
+
+def displayBoth(a, b):
+    c = []
+    for i in range(len(a)):
+        c.append(a[1]+b[1])
+    display(c)
--- subComp.py
+++ subComp.py
@@ -1,21 +1,29 @@
 import answerTen as aT
 import random as r
-from display import display
+import display as d
 
 def play():
     size, attempts, hits = 5, 10, 0
     answer = r.choice(aT.tenGen(size))
-    blank = [[], ['   A', 'B', 'C', 'D', 'E'], [],
-             ['1 ', '.', '.' ,'.', '.', '.'],
-             ['2 ', '.', '.' ,'.', '.', '.'],
-             ['3 ', '.', '.' ,'.', '.', '.'],
-             ['4 ', '.', '.' ,'.', '.', '.'],
-             ['5 ', '.', '.' ,'.', '.', '.'], []]
-    guess(size, attempts, hits, answer, blank)
+    blankPlayer = [['   PLAYER   '], [],
+                   ['   A', 'B', 'C', 'D', 'E'], [],
+                   ['1 ', '.', '.' ,'.', '.', '.'],
+                   ['2 ', '.', '.' ,'.', '.', '.'],
+                   ['3 ', '.', '.' ,'.', '.', '.'],
+                   ['4 ', '.', '.' ,'.', '.', '.'],
+                   ['5 ', '.', '.' ,'.', '.', '.'], []]
+    blankComp = [['   COMP   '], [],
+                   ['   A', 'B', 'C', 'D', 'E'], [],
+                   ['1 ', '.', '.' ,'.', '.', '.'],
+                   ['2 ', '.', '.' ,'.', '.', '.'],
+                   ['3 ', '.', '.' ,'.', '.', '.'],
+                   ['4 ', '.', '.' ,'.', '.', '.'],
+                   ['5 ', '.', '.' ,'.', '.', '.'], []]
+    guess(size, attempts, hits, answer, blankPlayer)
 
-def guess(size, attempts, hits, answer, blank):
+def guess(size, attempts, hits, answer, blankPlayer):
     while attempts != 0:
-        display(blank)
+        d.display(blankPlayer)
         print("You have %s attempts left." % attempts)
         if attempts == 10:
             g = input("Enter your guess (eg. D2): ") #variable 'g' to not confuse with guess()
@@ -23,14 +31,14 @@ def guess(size, attempts, hits, answer, blank):
             g = input("Enter your guess: ")
         if (len(g)<2) or g[0].upper() not in ['A', 'B', 'C', 'D', 'E'] or (g[1] not in ['1', '2', '3', '4', '5']):
             print("\nINVALID GUESS. Try again.\n")
-            guess(size, attempts, hits, answer, blank)
+            guess(size, attempts, hits, answer, blankPlayer)
         else:
             x, y = guessIdentify(g)
-            if blank[y+3][x+1] != '.': # to navigate "filler" text for display to work properly
+            if blankPlayer[y+3][x+1] != '.': # to navigate "filler" text for display to work properly
                 print("\nYou already guessed here. Try again.\n")
-                guess(size, attempts, hits, answer, blank)
+                guess(size, attempts, hits, answer, blankPlayer)
             else:
-                blank[y+3][x+1] = answer[x][y]
+                blankPlayer[y+3][x+1] = answer[x][y]
                 if answer[x][y] == "O":
                     print("\nMM    MM IIIIII  SSSS   SSSS \nMMM  MMM   II   SS  SS SS  SS\nMMMMMMMM   II    SS     SS   \nMM MM MM   II      SS     SS \nMM    MM   II   SS  SS SS  SS\nMM    MM IIIIII  SSSS   SSSS \n")
                 elif answer[x][y] == "X":
@@ -40,19 +48,19 @@ def guess(size, attempts, hits, answer, blank):
                     print("ERROR") #should never run, but just in case
                 attempts -= 1
         if hits == 3:
-            display(blank)
+            d.display(blankPlayer)
             print("\nYY  YY  OOOO  UU  UU     WW    WW IIIIII NN  NN !!\nYY  YY OO  OO UU  UU     WW    WW   II   NNN NN !!\n YYYY  OO  OO UU  UU     WW WW WW   II   NNNNNN !!\n  YY   OO  OO UU  UU     WWWWWWWW   II   NN NNN !!\n  YY   OO  OO UU  UU     WWW  WWW   II   NN  NN   \n  YY    OOOO   UUUU      WW    WW IIIIII NN  NN !!\n")
             again()
-    display(blank)
+    d.display(blankPlayer)
     print("\nYY  YY  OOOO  UU  UU     LL      OOOO   SSSS  EEEEEE\nYY  YY OO  OO UU  UU     LL     OO  OO SS  SS EE    \n YYYY  OO  OO UU  UU     LL     OO  OO  SS    EEEE  \n  YY   OO  OO UU  UU     LL     OO  OO    SS  EE    \n  YY   OO  OO UU  UU     LL     OO  OO SS  SS EE    \n  YY    OOOO   UUUU      LLLLLL  OOOO   SSSS  EEEEEE\n")
     print("The boat was here:\n")
     for i in range(5):
         for j in range(5):
             if answer[i][j] == "X":
-                blank[j+3][i+1] = answer[i][j]
+                blankPlayer[j+3][i+1] = answer[i][j]
             else:
                 continue
-    display(blank)
+    d.display(blankPlayer)
 
 def guessIdentify(g):
     guessLocation = [0, 0] #default is A1
