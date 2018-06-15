ab2d8113dfd7fdb3c4d5efbcf2c36e1b2d9038ff
Sam Crawford <sam.crawford82@gmail.com>
2018-06-14 20:22:00 -0400

    Added quit functionality to submarine and make playSingle() and againSingle()

--- submarine.py
+++ submarine.py
@@ -11,47 +11,38 @@ def play():
              ['3 ', '.', '.' ,'.', '.', '.'],
              ['4 ', '.', '.' ,'.', '.', '.'],
              ['5 ', '.', '.' ,'.', '.', '.'], []]
-    guess(size, attempts, hits, answer, blank)
+    return size, attempts, hits, answer, blank
 
-def guess(size, attempts, hits, answer, blank):
-    while attempts != 0:
-        display(blank)
-        print("You have %s attempts left." % attempts)
-        if attempts == 10:
-            g = input("Enter your guess (eg. D2): ") #variable 'g' to not confuse with guess()
-        else:
-            g = input("Enter your guess: ")
-        if (len(g)<2) or g[0].upper() not in ['A', 'B', 'C', 'D', 'E'] or (g[1] not in ['1', '2', '3', '4', '5']):
-            print("\nINVALID GUESS. Try again.\n")
-            guess(size, attempts, hits, answer, blank)
-        else:
-            x, y = guessIdentify(g)
-            if blank[y+3][x+1] != '.': # to navigate "filler" text for display to work properly
-                print("\nYou already guessed here. Try again.\n")
-                guess(size, attempts, hits, answer, blank)
-            else:
-                blank[y+3][x+1] = answer[x][y]
-                if answer[x][y] == "O":
-                    print("\nMM    MM IIIIII  SSSS   SSSS \nMMM  MMM   II   SS  SS SS  SS\nMMMMMMMM   II    SS     SS   \nMM MM MM   II      SS     SS \nMM    MM   II   SS  SS SS  SS\nMM    MM IIIIII  SSSS   SSSS \n")
-                elif answer[x][y] == "X":
-                    print("\nHH  HH IIIIII TTTTTT !!\nHH  HH   II     TT   !!\nHHHHHH   II     TT   !!\nHH  HH   II     TT   !!\nHH  HH   II     TT     \nHH  HH IIIIII   TT   !!\n")
-                    hits += 1
-                else:
-                    print("ERROR") #should never run, but just in case
-                attempts -= 1
-        if hits == 3:
-            display(blank)
-            print("\nYY  YY  OOOO  UU  UU     WW    WW IIIIII NN  NN !!\nYY  YY OO  OO UU  UU     WW    WW   II   NNN NN !!\n YYYY  OO  OO UU  UU     WW WW WW   II   NNNNNN !!\n  YY   OO  OO UU  UU     WWWWWWWW   II   NN NNN !!\n  YY   OO  OO UU  UU     WWW  WWW   II   NN  NN   \n  YY    OOOO   UUUU      WW    WW IIIIII NN  NN !!\n")
+def guess(attempts, hits, answer, blank):
     display(blank)
-    print("\nYY  YY  OOOO  UU  UU     LL      OOOO   SSSS  EEEEEE\nYY  YY OO  OO UU  UU     LL     OO  OO SS  SS EE    \n YYYY  OO  OO UU  UU     LL     OO  OO  SS    EEEE  \n  YY   OO  OO UU  UU     LL     OO  OO    SS  EE    \n  YY   OO  OO UU  UU     LL     OO  OO SS  SS EE    \n  YY    OOOO   UUUU      LLLLLL  OOOO   SSSS  EEEEEE\n")
-    print("The boat was here:\n")
-    for i in range(5):
-        for j in range(5):
-            if answer[i][j] == "X":
-                blank[j+3][i+1] = answer[i][j]
+    print("You have %s attempts left." % attempts)
+    if attempts == 10:
+        g = input("Enter your guess (eg. D2): ") #variable 'g' to not confuse with guess()
+    else:
+        g = input("Enter your guess: ")
+    if g.lower() in ['menu', 'quit', 'back', 'kill', 'no', 'nope', 'exit']:
+        attempts = 'menu'
+        return attempts, hits, answer, blank
+    elif (len(g)<2) or g[0].upper() not in ['A', 'B', 'C', 'D', 'E'] or (g[1] not in ['1', '2', '3', '4', '5']):
+        print("\nINVALID GUESS. Try again.\n")
+        guess(attempts, hits, answer, blank)
+    else:
+        x, y = guessIdentify(g)
+        if blank[y+3][x+1] != '.': # to navigate "filler" text for display to work properly
+            print("\nYou already guessed here. Try again.\n")
+            guess(attempts, hits, answer, blank)
+        else:
+            blank[y+3][x+1] = answer[x][y]
+            if answer[x][y] == "O":
+                print("\nMM    MM IIIIII  SSSS   SSSS \nMMM  MMM   II   SS  SS SS  SS\nMMMMMMMM   II    SS     SS   \nMM MM MM   II      SS     SS \nMM    MM   II   SS  SS SS  SS\nMM    MM IIIIII  SSSS   SSSS \n")
+            elif answer[x][y] == "X":
+                print("\nHH  HH IIIIII TTTTTT !!\nHH  HH   II     TT   !!\nHHHHHH   II     TT   !!\nHH  HH   II     TT   !!\nHH  HH   II     TT     \nHH  HH IIIIII   TT   !!\n")
+                hits += 1
             else:
-                continue
-    display(blank)
+                print("ERROR") #should never run, but just in case
+            attempts -= 1
+    return attempts, hits, answer, blank
+    
 
 def guessIdentify(g):
     guessLocation = [0, 0] #default is A1
