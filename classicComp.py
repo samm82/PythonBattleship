135b1e1a4afc698f0144775edccb9444a3e00652
Sam Crawford <sam.crawford82@gmail.com>
2018-06-27 15:46:13 -0400

    Added print(hits) for testing - sometimes comp gives up on partially sunk ships

--- classicComp.py
+++ classicComp.py
@@ -100,6 +100,7 @@ def guess(p, answer, player, comp, cL):
     theListTM = ["A1","A2","A3","A4","A5","A6","A7","A8","A9","A10","B1","B2","B3","B4","B5","B6","B7","B8","B9","B10","C1","C2","C3","C4","C5","C6","C7","C8","C9","C10","D1","D2","D3","D4","D5","D6","D7","D8","D9","D10","E1","E2","E3","E4","E5","E6","E7","E8","E9","E10","F1","F2","F3","F4","F5","F6","F7","F8","F9","F10","G1","G2","G3","G4","G5","G6","G7","G8","G9","G10","H1","H2","H3","H4","H5","H6","H7","H8","H9","H10","I1","I2","I3","I4","I5","I6","I7","I8","I9","I10","J1","J2","J3","J4","J5","J6","J7","J8","J9","J10"]
     g = theListTM.pop(r.randrange(len(theListTM)))
     input(g)
+    # ^^REMOVE ^^^
     if (len(g)>1) and g.lower() in ['menu', 'quit', 'back', 'kill', 'no', 'nope', 'exit']:
         return "menu"
     elif (len(g)<2) or (g[0].upper() not in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']) or (g[1:] not in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']):
@@ -123,7 +124,8 @@ def guess(p, answer, player, comp, cL):
     return [p, answer, player, comp, cL]
 
 def compGuess(c, comp, gL, tryHere, hits, diff):
-    print(tryHere)
+    print("Hits:", hits)
+    print("Try:", tryHere)
     if len(hits) >= 2 and (diff == 2):
         tryHere = genTryFromHits(comp, tryHere, hits)
     if diff >= 1 and tryHere:
@@ -183,7 +185,8 @@ def compGuess(c, comp, gL, tryHere, hits, diff):
         comp[y+5][x+1] = "X"
     else:
         return None
-    print(tryHere)
+    print("Hits:", hits)
+    print("Try:", tryHere)
     return c, comp, gL, tryHere, hits
 
 def checkBoats(boats, guess, cL):
